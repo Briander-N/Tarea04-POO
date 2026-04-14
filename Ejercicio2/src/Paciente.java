9033ac22391a9a7be045939b865cc1f1bc6af53d
Briander <pochitobriander@gmail.com>
2026-04-14 18:57:01 -0500

    Ej2 metodos y getters generados y aplicados

--- Ejercicio2/src/Paciente.java
+++ Ejercicio2/src/Paciente.java
@@ -38,4 +38,62 @@ public class Paciente {
         }
     }
 
+    public int getEdad() {
+        return edad;
+    }
+
+    public double getCostoConsulta() {
+        return costoConsulta;
+    }
+
+    public double calcularCostoTotal(){
+        return getCostoConsulta() * numeroConsultas;
+    }
+
+    public double calcularDescuento(){
+        double total = calcularCostoTotal();
+
+        if(tipoSeguro == "Basico"){
+            System.out.println("Descuento del 10%");
+            return  total - (total * 0.10);
+        }else if(tipoSeguro == "Premium"){
+            System.out.println("Descuento del 20%");
+            return total - (total * 0.20);
+        }else{
+            System.out.println("No aplica descuento");
+            return total;
+        }
+    }
+    public String clasificacionPrioridad() {
+        if (prioridad >= 4){
+            return "Alta prioridad";
+        }else if(prioridad >= 2) {
+            return "Prioridad media";
+        } else{
+            return "Prioridad baja";
+        }
+    }
+    public String mensajeMedico() {
+        String clasificacion = clasificacionPrioridad();
+
+        if (clasificacion == "Alta prioridad"){
+            return "Atención inmediata requerida";
+        }else if (clasificacion == "Prioridad media"){
+            return "Seguimiento necesario";
+        } else {
+            return "Control regular";
+        }
+    }
+    public void mostrarReporte() {
+        System.out.println("===== REPORTE MÉDICO =====");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("ID: " + id);
+        System.out.println("Seguro: " + tipoSeguro);
+        System.out.println("Edad: " + getEdad());
+        System.out.println("Consultas: " + numeroConsultas);
+        System.out.println("Costo total: " + calcularCostoTotal());
+        System.out.println("Precio con Descuento: " + calcularDescuento());
+        System.out.println("Prioridad: " + clasificacionPrioridad());
+        System.out.println("Mensaje: " + mensajeMedico());
+    }
 }
